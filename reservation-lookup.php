4bf506c0351a43bed59f15443e29746e23a39a8e
Chia Wen Cheng <r4n0b@bowen.ugrad.cs.ubc.ca>
2016-03-31 09:58:55 -0700

    reservation-lookup

---
+++ reservation-lookup.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Reservation lookup</title>
+    <link rel="stylesheet" href="css/style.css">
+    <link href='http://fonts.googleapis.com/css?family=Lato:400,700' rel='stylesheet' type='text/css'>
+  </head>
+  <body>
+    <form action="./confirmation.php" metho.d="post" name="reservation_lookup">
+      <div class="header">
+         <p>Reservation Look Up</p>
+      </div>
+      <div class="description">
+        <p>View, change or cancel your hotel reservation</p>
+      </div>
+      <div class="input">
+        <input type="text" class="button" id="conf_no" name="conf_no" placeholder="Confirmation Number(10 digits)" 
+        pattern=".{10,10}" title="10 digits" required>
+        <input type="submit" class="button" id="submit" value="View Reservation">
+      </div>
+    </form>
+  </body>
+</html>
