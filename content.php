d01acf4caa69ce74a9b80cbd468d3537b8618544
Parynaz Adamy <p.adamy@live.ca>
2016-04-03 16:09:01 -0700

    Added reservation pages

--- content.php
+++ content.php
@@ -1,6 +1,9 @@
 <html>
 <body>
-<h>Thanks for Booking! Please enter your information below: </h>
+<link rel="stylesheet" type="text/css" href="css/content.css">
+
+
+<h1>Thank you for Booking! Please enter your information below: </h1>
 
 <?php 
 
@@ -8,39 +11,55 @@ $startDate = $_GET['start-date'];
 $endDate = $_GET['end-date'];
 $room_no =$_GET['room-no'];
 
-echo $startDate;
-echo $endDate;
-echo $room_no;
 
 ?>
 
+<table style="width:100%">
+  <tr>
+    <th>Dates Requested</th>
+    <th>Total</th>
+  </tr>
+  <tr>
+    <td><center><?php echo $startDate?> - <?php echo $endDate?></center></td>
+    <td><center>Number of Nights: <br>
+    			Room Rate: <br>
+    			Total : </center></td> 
+  </tr>
+</table>
+
 
 <form action="reservation.php" method="post">
-Name: <input type="text" name="name"><br>
-Age: <input type="text" name="age"><br>
-Street: <input type="text" name="street"><br>
-Zipcode: <input type="text" name="zipcode"><br>
-<h1> Reservation Confirmation </h1>
-Confermation Number:<input type="text" name="conf_no"><br>
 
-Room Number: <?php echo $room_no ?>
+<input type = "hidden" name = 'start-date' value = <?php echo $startDate ?>><br>
+<input type = "hidden" name = 'end-date' value = <?php echo $endDate ?>><br>
+<input type = "hidden" name = 'room_no' value = <?php echo $room_no ?>><br>
 
-<!--<input type="text" name="room_no"><br> -->
+<b>Name: </b><input type="text" name="name" required><br>
+<b>Age:</b> <input type="text" name="age" required><br>
+<b>Street:</b> <input type="text" name="street" required><br>
+<b>Zipcode:</b> <input type="text" value = "XXX XXXX"name="zipcode" required><br>
 
-Name on Card: <input type="text" name="card_name"><br>
-Card Type: <input type="text" name="card_type"><br>
-Card Number: <input type="text" name="card_no"><br>
 
-Expirary Date (DD-MON-YY): <input type="text" name="exp_date"><br>
-Added Date: <input type="text" name="added_date"><br>
+<b>Name on Card:</b> <input type="text" name="card_name" required><br>
+
+<b>Card Type: 	</b><select name = "card_type" required>
+		<option value = "VISA"> VISA </option>
+		<option value = "MasterCard">MASTERCARD </option>
+		<option value = "American Express">American Express</option>
+		</select><br>
+
+<b>Card Number:</b> <input type="text" name="card_no" required><br>
+
+<b>Expirary Date:</b> <input type="text" value = 'DD-Mon-YY'name="exp_date" required><br>
+<!-- Added Date: <input type="text" name="added_date"><br>
 CheckIn Time: <input type="text" name="checkin_timestamp"><br>
+ -->
 
+<b>Phone: </b><input type="text" value = "xxx-xxx-xxxx" name="phone" required><br>
+<br>
 
-Phone: <input type="text" value = "xxx-xxx-xxxx" name="phone"><br>
-
-From: <input type="text" name="from_date"><br>
-To: <input type="text" name="to_date"><br>
-
+<input type="checkbox" value="Bike" required>I confirm that the information above is correct<br>
+ <br>
 
 <input type="submit">
 </form>
