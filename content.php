ae803ae7f95c6c0a9d72b4da75ce7bcd5a03ce7c
colleenv <colleenvuong@gmail.com>
2016-04-03 19:37:46 -0700

    fixed search query & updated css for content/reservation

--- content.php
+++ content.php
@@ -1,9 +1,42 @@
-<html>
+<html lang="en" class="no-js">
+
+<head>
+  <meta charset="UTF-8">
+  <title>304 Hotel</title>
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+
+  <!-- Font-->
+  <link rel='stylesheet' type='text/css' href='http://fonts.googleapis.com/css?family=Roboto:400,100,300,500,700,900' >
+
+  <!-- Stylesheets -->
+  <link rel="stylesheet" href="http://code.jquery.com/ui/1.11.4/themes/smoothness/jquery-ui.css">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
+  <link rel="stylesheet" type="text/css" href="css/bootstrap.css">
+  <link rel="stylesheet" type="text/css" media="all" href="css/template.css" >
+  <link rel="stylesheet" type="text/css" media="all" href="css/magnific-popup.css" >
+  <link rel="stylesheet" type="text/css" href="css/bootstrap-responsive.css">
+
+
+<!-- Javscripts -->
+  <script type="text/javascript" src="http://code.jquery.com/jquery-1.12.0.min.js"></script>
+  <script type="text/javascript" src="http://code.jquery.com/ui/1.11.4/jquery-ui.js"></script>
+  <script type="text/javascript" src="js/jquery.magnific-popup.js"></script>
+  <script type="text/javascript" src="js/scripts.js"></script>
+
+</head>
+
+
 <body>
-<link rel="stylesheet" type="text/css" href="css/content.css">
 
+<!-- Top Header / Header Bar -->
+<div id="home" class="boxed-view">
+    <?php include("header.html");?>
 
-<h1>Thank you for Booking! Please enter your information below: </h1>
+<section class="box">
+<div class="container">
+
+<h4 align='center'>Thanks for Booking!<h4>
+  <h3 align='center'>Please enter your information below: </h3>
 
 <?php 
 
@@ -14,7 +47,10 @@ $room_no =$_GET['room-no'];
 
 ?>
 
-<table style="width:100%">
+
+<div class="row">
+  <div class="col-md-12 col-sm-6">
+    <table class='display' cellspacing='0' max-width='100%' >
   <tr>
     <th>Dates Requested</th>
     <th>Total</th>
@@ -22,12 +58,16 @@ $room_no =$_GET['room-no'];
   <tr>
     <td><center><?php echo $startDate?> - <?php echo $endDate?></center></td>
     <td><center>Number of Nights: <br>
-    			Room Rate: <br>
-    			Total : </center></td> 
+          Room Rate: <br>
+          Total : </center></td> 
   </tr>
 </table>
+</div>
+</div>
 
 
+<div class="row">
+<div class="col-md-12 col-sm-6">
 <form action="reservation.php" method="post">
 
 <input type = "hidden" name = 'start-date' value = <?php echo $startDate ?>><br>
@@ -37,16 +77,16 @@ $room_no =$_GET['room-no'];
 <b>Name: </b><input type="text" name="name" required><br>
 <b>Age:</b> <input type="text" name="age" required><br>
 <b>Street:</b> <input type="text" name="street" required><br>
-<b>Zipcode:</b> <input type="text" value = "XXX XXXX"name="zipcode" required><br>
+<b>Zipcode:</b> <input type="text" value = "XXX XXX"name="zipcode" required><br>
 
 
 <b>Name on Card:</b> <input type="text" name="card_name" required><br>
 
-<b>Card Type: 	</b><select name = "card_type" required>
-		<option value = "VISA"> VISA </option>
-		<option value = "MasterCard">MASTERCARD </option>
-		<option value = "American Express">American Express</option>
-		</select><br>
+<b>Card Type:   </b><select name = "card_type" required>
+    <option value = "VISA"> VISA </option>
+    <option value = "MasterCard">MASTERCARD </option>
+    <option value = "American Express">American Express</option>
+    </select><br>
 
 <b>Card Number:</b> <input type="text" name="card_no" required><br>
 
@@ -55,14 +95,24 @@ $room_no =$_GET['room-no'];
 CheckIn Time: <input type="text" name="checkin_timestamp"><br>
  -->
 
-<b>Phone: </b><input type="text" value = "xxx-xxx-xxxx" name="phone" required><br>
+<b>Phone: </b><input type="text" value = "XXX-XXX-XXXX" name="phone" required><br>
 <br>
 
-<input type="checkbox" value="Bike" required>I confirm that the information above is correct<br>
+<center><input type="checkbox" value="Bike" required>I confirm that the information above is correct<br></center>
  <br>
 
-<input type="submit">
-</form>
+<center><button class="btn button-md green hover-dark-green soft-corners" type="submit">Submit</button></center>
+</form></div>
+
+</div>
+</div>
+
+</section>
+
+<?php include("footer.html");?>
 
 </body>
+
+
+
 </html>
--- reservation.php
+++ reservation.php
@@ -1,21 +1,55 @@
+<html lang="en" class="no-js">
 
-<html>
-<link rel="stylesheet" type="text/css" href="css/content.css">
+<head>
+  <meta charset="UTF-8">
+  <title>304 Hotel</title>
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+
+  <!-- Font-->
+  <link rel='stylesheet' type='text/css' href='http://fonts.googleapis.com/css?family=Roboto:400,100,300,500,700,900' >
+
+  <!-- Stylesheets -->
+  <link rel="stylesheet" href="http://code.jquery.com/ui/1.11.4/themes/smoothness/jquery-ui.css">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
+  <link rel="stylesheet" type="text/css" href="css/bootstrap.css">
+  <link rel="stylesheet" type="text/css" media="all" href="css/template.css" >
+  <link rel="stylesheet" type="text/css" media="all" href="css/magnific-popup.css" >
+  <link rel="stylesheet" type="text/css" href="css/bootstrap-responsive.css">
+
+
+<!-- Javscripts -->
+  <script type="text/javascript" src="http://code.jquery.com/jquery-1.12.0.min.js"></script>
+  <script type="text/javascript" src="http://code.jquery.com/ui/1.11.4/jquery-ui.js"></script>
+  <script type="text/javascript" src="js/jquery.magnific-popup.js"></script>
+  <script type="text/javascript" src="js/scripts.js"></script>
+
+</head>
+
 
 <body>
 
-Thank you for booking with us, <b><?php echo $_POST["name"]; ?></b><br>
-Here is your confirmation number:<b> <?php 
+<!-- Top Header / Header Bar -->
+<div id="home" class="boxed-view">
+    <?php include("header.html");?>
+
+<section class="box">
+<div class="container">
+
+<h4 align='center'>Thank you for booking with us, <b><?php echo $_POST["name"]; ?></b> </h4>
+ <h3 align='center'>Here is your confirmation number:<b> 
+
+<?php 
 
 $conf = rand(); 
 $confn = sprintf('%10d', $conf);
 echo $confn; 
 ?></b><br>
+</h3>
 
 Please use your confirmation number to log in to view your reservation at a later time. <br>
 Below you will find your reservation confirmation details:<br><br>
 
-Reservation Details:<br>
+<font color='#4fabbc'>Reservation Details:</font><br>
 <table style = "width: 100%" border = "2">
 <tr>
   <th>Room Number</th>
@@ -29,7 +63,7 @@ Reservation Details:<br>
 </tr>
 </table>
 
-Customer Confirmation:<br>
+<font color='#4fabbc'>Customer Confirmation:</font><br>
 
 <?php
 $success = True; //keep track of errors so it redirects the page only if there are no errors
@@ -105,7 +139,7 @@ oci_free_statement($after);
 
 ?>
 
-Reservation Confirmation: <br>
+<font color='#4fabbc'>Reservation Confirmation:</font><br>
 
 <?php
 $success = True; //keep track of errors so it redirects the page only if there are no errors
@@ -215,5 +249,10 @@ oci_close($conn);
   })
   </script>
 
+</div>
+<?php include("footer.html");?>
 </body>
+
+
+
 </html>
